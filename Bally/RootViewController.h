56a35c4fe6f732d0a81bb65615c6d49aecd570f0
Your Name <fmemon100@yahoo.com>
2012-03-22 16:17:52 -0400

    Initial Commit

---
+++ Bally/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Bally
+//
+//  Created by Saida Memon on 3/22/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
